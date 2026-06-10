83a5f7c3ed1aadf6c74739ffa5b3d5300898a1d5
K.Vamshi Krishna <Vk676343@gmail.com>
2026-06-10 13:19:06 +0530

    upchange

--- routes/listing.js
+++ routes/listing.js
@@ -44,7 +44,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 //Create Route
 
-router.post("/",validateListing, wrapAsync(async (req, res, next) => {
+router.post("/", isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
 
       
     const newListing = new Listing(req.body.listing);
